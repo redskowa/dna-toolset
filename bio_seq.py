d0884a1f5e4ada90a368d51283f275b2c8a6f248
redskowa <redskowa@gmail.com>
2023-02-24 14:22:35 -0700

    Toolkit updated into a class, added RNA support

--- bio_seq.py
+++ bio_seq.py
@@ -1,26 +1,162 @@
-from bio_structs import *
+from collections import Counter
+import random
+
+from bio_structs import NUCLEOTIDE_BASE, DNA_Codons, RNA_Codons
 
 class bio_seq:
-    '''DNA sequence class. Default value: ATCG, DNA, No Label'''
+    """DNA sequence class. Default value: ATCG, DNA, No Label"""
 
     def __init__(self, seq="ATCG", seq_type="DNA", label="No Label"):
-        '''Sequence initialization, validation'''
+        """Sequence initialization, validation"""
         self.seq = seq.upper()
         self.label = label
         self.seq_type = seq_type
         self.is_valid = self.__validate()
         assert self.is_valid, f"Provided data does not seem to be correct {self.seq_type} sequence"
-    
-    def validate(self):
-        '''Check the sequence to make sure it is a valid DNA string'''
-        return set(DNA_Nucleotides).issuperset(self.seq)
 
+    # DNA Toolkit functions
+    def __validate(self):
+        """Check the sequence to make sure it is a valid DNA string"""
+        return set(NUCLEOTIDE_BASE[self.seq_type]).issuperset(self.seq)
 
     def get_seq_biotype(self):
-        '''Returns sequence type'''
+        """Returns sequence type"""
         return self.seq_type
 
-    
     def get_seq_info(self):
-        '''Returns 4 strings. Full sequence information'''
+        """Returns 4 strings. Full sequence information"""
         return f"[Label]: {self.label}\n[Sequence]: {self.seq}\n[Biotype]: {self.seq_type}\n[Length]: {len(self.seq)}"
+
+    def generate_rnd_seq(self, length=10, seq_type="DNA"):
+        """Generate a random DNA or RNA sequence, provided the length"""
+        seq = ''.join([random.choice(NUCLEOTIDE_BASE[seq_type])
+                       for x in range(length)])
+        self.__init__(seq, seq_type, "Randomly generated sequence")
+
+    # === Nucleotide Frequency Counter === #
+    def count_nuc_frequency(self):
+        """Counting nucleotide frequency of a given sequence."""
+        return dict(Counter(self.seq))
+
+    # === Transcription === #
+    def transcription(self):
+        """DNA -> RNA Transcription, Replacing Thymine with Uracil"""
+        if self.seq_type == "DNA":
+            return self.seq.replace("T", "U")
+        return "Not a DNA sequence"
+
+    # === Complement === #
+    def complement(self):
+        """Swapping adenine with thymine and guanine with cytosine. 3' to 5' order"""
+        #return ''.join([DNA_Complement[nuc] for nuc in self.seq])
+        if self.seq_type == "DNA":
+            mapping = str.maketrans('ATCG', 'TAGC')
+        else:
+            mapping = str.maketrans('AUCG', 'UAGC')
+        return self.seq.translate(mapping)
+
+    # === Reverse Complement === #
+    def reverse_complement(self):
+        '''Swapping adenine with thymine and guanine with cytosine. Then reversing generated string. 5' to 3' order'''
+        # return ''.join([DNA_Complement[nuc] for nuc in self.seq])[::-1]
+
+        #pythonic solution using built in .maketrans function
+        if self.seq_type == "DNA":
+            mapping = str.maketrans('ATCG', 'TAGC')
+        else:
+            mapping = str.maketrans('AUCG', 'UAGC')
+        return self.seq.translate(mapping)[::-1]
+
+    ## ==== GC Content ==== ##
+    def gc_content(self):
+        '''GC content ration in a DNA/RNA sequence'''
+        return round((self.seq.count('C') + self.seq.count('G')) / len(self.seq) * 100, 3)
+
+    def gc_content_subsec(self, k=20):
+        '''GC Content in a DNA/RNA sub-sequence length k. k = 20 by default'''
+        res = []
+        for i in range(0, len(self.seq) - k + 1, k):
+            subseq = self.seq[i:i+k]
+            res.append(
+                round((subseq.count('C') + subseq.count('G')) / len(subseq) * 100, 3)
+                )
+        return res
+
+    def translate_seq(self, init_pos=0):
+        '''Translates a DNA sequence into an amino acid sequence'''
+        if self.seq_type == "DNA":
+            return [DNA_Codons[self.seq[pos:pos+3]] for pos in range(init_pos, len(self.seq) -2, 3)]
+        elif self.seq_type == "RNA":
+            return [RNA_Codons[self.seq[pos:pos+3]] for pos in range(init_pos, len(self.seq) -2, 3)]
+
+    def codon_usage(self, aminoacid):
+        '''Provides the frequency of each codon encoding a given aminoacid in a DNA sequence'''
+        tmpList = []
+        if self.seq_type == "DNA":
+            for i in range(0, len(self.seq) -2, 3):
+                if DNA_Codons[self.seq[i:i+3]] == aminoacid:
+                    tmpList.append(self.seq[i:i+3])
+        elif self.seq_type == "RNA":
+            for i in range(0, len(self.seq) -2, 3):
+                if RNA_Codons[self.seq[i:i+3]] == aminoacid:
+                    tmpList.append(self.seq[i:i+3])
+        
+        freqDict = dict(Counter(tmpList))
+        totalWight = sum(freqDict.values())
+        for self.seq in freqDict:
+            freqDict[self.seq] = round(freqDict[self.seq] / totalWight, 2)
+        return freqDict
+
+    def gen_reading_frames(self):
+        """Generate the six reading frames of a DNA sequence, including the reverse complement"""
+        frames = []
+        frames.append(self.translate_seq(0))
+        frames.append(self.translate_seq(1))
+        frames.append(self.translate_seq(2))
+        tmp_seq = bio_seq(self.reverse_complement(), self.seq_type)
+        frames.append(tmp_seq.translate_seq(0))
+        frames.append(tmp_seq.translate_seq(1))
+        frames.append(tmp_seq.translate_seq(2))
+        del tmp_seq
+        return frames
+
+    def proteins_from_rf(self, aa_seq):
+        '''Compute all possible proteins in an aminoacid seq and return a list of possible proteins'''
+        current_prot = []
+        proteins = []
+        for aa in aa_seq:
+            if aa == "_":
+                # STOP accumulating amino acids if _ - STOP was found
+                if current_prot:
+                    for p in current_prot:
+                        proteins.append(p)
+                    current_prot = []
+            else:
+                # START accumulating amino acids if M - START was found
+                if aa == "M":
+                    current_prot.append("")
+                for i in range (len(current_prot)):
+                    current_prot[i] += aa
+        return proteins
+
+    def all_proteins_from_open_rfs(self, startReadPos=0, endReadPos=0, ordered=False):
+        '''Compute all possible proteins for all open reading frames'''
+        '''Protein search DB: https://www.ncbi.nlm.nih.gov/nucore/NM_001185097.2'''
+        '''API can be used to pull protein info'''
+        if endReadPos > startReadPos:
+            tmp_seq = bio_seq(self.seq[startReadPos: endReadPos], self.seq_type)
+            rfs = tmp_seq.gen_reading_frames()
+            del tmp_seq
+        else:
+            rfs = self.gen_reading_frames()
+
+        result = []
+        for rf in rfs:
+            prots = self.proteins_from_rf(rf)
+            for p in prots:
+                result.append(p)
+        
+        if ordered:
+            return sorted(result, key=len, reverse=True)
+        
+        return result
--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 # DNA Toolset/Code testing file
 
 from bio_seq import bio_seq
+from utilities import readTextFile, writeTextFile, read_FASTA
 
-test_dna = bio_seq("ATTGGCAGGTACGTAGGC", "DNA", "Test Label")
+test_dna = bio_seq()
+test_dna.generate_rnd_seq(40, "RNA")
+
 
 print(test_dna.get_seq_info())
-print(test_dna.get_seq_biotype())
+print(f'[Nucleotide Frequency]: {test_dna.count_nuc_frequency()}')
+print(f'[DNA/RNA Transcription]: {test_dna.transcription()}\n')
 
+print(f"[DNA String + Complement]:\n\n5' {test_dna.seq} 3'")
+print(f"   {''.join(['|' for c in range(len(test_dna.seq))])}")
+print(f"3' {test_dna.complement()} 5' [Complement]")
+
+print(f"5' {test_dna.reverse_complement()} 3' [Reverse Complement]\n")
+print(f"[GC Content]: {test_dna.gc_content()}%\n")
+print(f"[GC Content in Subsection k=20: {test_dna.gc_content_subsec()}\n")
+print(f'[Aminoacids Sequence]: {test_dna.translate_seq()}\n')
+print(f'[Codon Frequency (L): {test_dna.codon_usage("L")}\n')
+print('[Reading Frames]:')
+for frame in test_dna.gen_reading_frames():
+    print(frame)
+print('\n[All proteins in 6 open reading frames]:')
+for prot in test_dna.all_proteins_from_open_rfs():
+    print(f'{prot}')
